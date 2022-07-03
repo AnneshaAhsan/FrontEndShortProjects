2f2afbedb921b04c9699395ad09ee2a512b48e82
AnneshaAhsan <94369767+AnneshaAhsan@users.noreply.github.com>
2022-07-03 21:06:03 +0600

    Add files via upload

---
+++ Project 5 Content Placeholder/script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
+  title.innerHTML = 'Web Developer Profile'
+  excerpt.innerHTML =
+    'Welcome to my website! I am Annesha Ahsan and I like creating Idealistic Web Pages'
+  profile_img.innerHTML =
+    '<img src="profile.jpg" alt="" />'
+  name.innerHTML = 'Annesha Ahsan'
+  date.innerHTML = 'Oct 15, 1998'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
